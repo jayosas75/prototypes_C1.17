4bd5fdecdbffd9a91c85235360705e5d335b7283
John Yosas <jayosas75@gmail.com>
2017-01-31 14:11:13 -0800

    js ajax - John Yosas

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,5 +1,7 @@
 //Create GLOBAL variable below here on line 2
-
+var global_result = null;
+var movie_image = null;
+var movie_info = null;
 
 $(document).ready(function(){
     $('button').click(function(){
@@ -9,9 +11,21 @@ $(document).ready(function(){
             url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
             success: function(result) {
                 console.log('AJAX Success function called, with the following result:', result);
-
+                global_result = result;
+                var first_movie_info = result.feed.entry[0]['im:image'][2].label;
+                console.log(first_movie_info);
+                for(var i = 0; i < result.feed.entry.length; i++){
+                    movie_image = result.feed.entry[i]['im:image'][2].label;
+                    var each_image = $('<img>', {src: movie_image});
+                    movie_info = "Director: " +result.feed.entry[i]['im:artist'].label + " Title: " +result.feed.entry[i].title.label;
+                    var new_div = $('<div>');
+                    var caption = $('<h2>').text(movie_info);
+                    new_div.append(each_image).append(caption);
+                    $('#main').append(new_div);
+                }
             }
         });
         console.log('End of click function');
     });
 });
+
